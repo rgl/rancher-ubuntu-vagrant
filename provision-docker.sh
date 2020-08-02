a91a484ccc6061499a56da23b01c2eb184988fbc
Rui Lopes <rgl@ruilopes.com>
2020-08-02 14:56:50 +0100

    remove unneeded apt-get update as its already done by add-apt-repository

--- provision-docker.sh
+++ provision-docker.sh
@@ -17,7 +17,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y apt-transport-https software-properties-common gnupg2
 wget -qO- https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
 apt-get install -y "docker-ce=$docker_version" "docker-ce-cli=$docker_version" containerd.io
 
 # configure docker to get docker hub images from our registry proxy.
